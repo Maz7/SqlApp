ffdd99051296e3a977a4ebfb5f3fa758f8aec766
Madazer <codermaz@outlook.com>
2016-01-11 19:32:04 +0000

    4th test

--- SqlApp/Program.cs
+++ SqlApp/Program.cs
@@ -11,7 +11,7 @@ namespace SqlApp
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        /// //commentttts
+        /// //comm
         [STAThread]
         static void Main()
         {
